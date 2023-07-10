ef6ae0c57ff040e5754499e9c77438443d70e112
MayraGarciaM <stefagarcia.23@hotmail.com>
2023-07-10 10:48:47 -0500

    se agrega barra de busqueda

--- src/App.jsx
+++ src/App.jsx
@@ -12,15 +12,19 @@ function App() {
   const [movies, setMovies] = useState([]);
   const [searchKey, setSearchKey] = useState("");
 
-  //Función para realizar para realizar la peticion por get a la api
+  //Función para realizar la peticion por get a la api
   const fetchMovies = async (searchKey) => {
     const type = searchKey ? "search" : "discover"
     const { data: { results }, } = await axios.get(`${API_URL}/${type}/movie?api_key=9076d9db918bca7952195c7fa46f29af`
     )
-    console.log("entro")
     setMovies(results)
   };
 
+ //Funciòn de busqueda de peliculas
+  const searchMovies = (e)=>{
+    e.preventDefault();
+    fetchMovies(searchMovies)
+  }
 
   useEffect(() => {
     fetchMovies();
@@ -28,13 +32,20 @@ function App() {
 
   return (
     <div>
+      <h2 className='text-center'>Movie Magic</h2>
+      {/*Buscador*/}
+      <form className='container mb-4' onSubmit={searchMovies}>
+        <input type="text" placeholder='search' onChange={(e)=> setSearchKey(e.target.value)} />
+        <button className='btn btn-primary'>Search</button>
+      </form>
+
       {/*contenedor que muestra peli actual*/}
-      <div className='container'>
+      <div className='container mt-3'>
         <div className='row'>
           {movies.map((movie) => (
-            <div key={movie.id}>
-              <img src={`${URL_image + movie.poster_path}`} alt="" />
-              <h4>{movie.tittle}</h4>
+            <div key={movie.id} className="col-md-4 mb-3">
+              <img src={`${URL_image + movie.poster_path}`} alt="" height={600} width="100%" />
+              <h4 className='text-center'>{movie.title}</h4>
             </div>
           ))}
         </div>
